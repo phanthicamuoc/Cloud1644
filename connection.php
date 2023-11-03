a5a6233ee4cc8652178908734f9a8a773500a37c
phan thi cam uoc <uocptcgcc210016@fpt.edu.vn>
2023-11-03 15:24:49 +0700

    kkk

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'iktdav436tsnp16x','woshxx238gacnlve', 'p1aqt0qgyimguvhe')
+$conn = mysqli_connect('dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'f0rhmftlrh127wzl','xe548x2afkenk0ka', 'v5jy8hrs7hrw7fsc')
         or die("Can not connect database".mysqli_connect_error());
 ?>
